8b9c9337c21e560e8d831addb0356f48280e24b9
Peter Rundqvist <peter.rundqvist@simrishamn.se>
2023-08-07 12:20:44 +0200

    Added constructor to FeedbackReportingService

--- src/Services/Deviation/FeedbackReporting.API/Grpc/FeedbackReportingService.cs
+++ src/Services/Deviation/FeedbackReporting.API/Grpc/FeedbackReportingService.cs
@@ -1,5 +1,19 @@
-﻿namespace Microsoft.eShopOnContainers.Services.Deviation.FeedbackReporting.API.Grpc;
+﻿using Microsoft.EntityFrameworkCore;
 
-public class FeedbackReportingService
+using static FeedbackReportingGrpcApi.FeedbackReportingGrpc;
+
+namespace Microsoft.eShopOnContainers.Services.Deviation.FeedbackReporting.API.Grpc;
+
+public class FeedbackReportingService : FeedbackReportingGrpcBase
 {
+    private readonly FeedbackReportingContext _feedbackReportingContext;
+    private readonly FeedbackReportingSettings _settings;
+    private readonly ILogger<FeedbackReportingService> _logger;
+
+    public FeedbackReportingService(FeedbackReportingContext dbContext, IOptions<FeedbackReportingSettings> settings, ILogger<FeedbackReportingService> logger)
+    {
+        _settings = settings.Value;
+        _feedbackReportingContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        _logger = logger;
+    }
 }
